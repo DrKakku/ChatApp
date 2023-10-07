9e62f20341ec6a186b813dcf594e83337dc9e77f
ytripathi <ytripathi@USHYDYTRIP09.us.deloitte.com>
2023-10-07 17:04:13 +0530

    base git project

---
+++ LocalChatApp/src/test/java/com/deloitte/springboot/chatapp/LocalChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.springboot.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
