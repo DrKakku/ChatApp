41d5711f908712dfe13b6a9a2947af804a2beaa7
Yash Tripathi <53153448+DrKakku@users.noreply.github.com>
2023-10-09 12:57:34 +0530

    Added auto browser opening feature
    Disable it by commenting it out in the main file

--- LocalChatApp/src/main/java/com/deloitte/springboot/chatapp/controller/ChatController.java
+++ LocalChatApp/src/main/java/com/deloitte/springboot/chatapp/controller/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,13 +28,13 @@ public class ChatController {
 	UserDao userD;
 	
 
-	@RequestMapping(value = "/chatHealth")
+	@RequestMapping(value = "/")
 	public ResponseEntity chatHealth()
 	{
 		return new ResponseEntity<>("The Chat API is healthy and running",HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/sendMessage",consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	@PostMapping(value = "/sendMessage",consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity sendMessage(@RequestBody ChatResponse ch)
 	{
 		
